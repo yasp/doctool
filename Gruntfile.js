f4556f13541d41ab6d072d0fb945895d1c0876c9
luto <m@luto.at>
2013-09-28 12:32:54 +0200

    added grunt boilerplate

---
+++ Gruntfile.js
@@ -0,0 +1,43 @@
+/*
+ * grunt-doctool
+ * https://github.com/yasp/doctool
+ *
+ * Copyright (c) 2013 yasp
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+
+    // Configuration to be run (and then tested).
+    doctool: {
+      default_options: {
+        options: {
+        },
+        files: {
+          'tmp/default_options': ['test/fixtures/testing', 'test/fixtures/123'],
+        },
+      },
+      custom_options: {
+        options: {
+          separator: ': ',
+          punctuation: ' !!!',
+        },
+        files: {
+          'tmp/custom_options': ['test/fixtures/testing', 'test/fixtures/123'],
+        },
+      },
+    },
+  });
+
+  // Actually load this plugin's task(s).
+  grunt.loadTasks('tasks');
+
+  // Whenever the "test" task is run, first clean the "tmp" dir, then run this
+  // plugin's task(s), then test the result.
+  grunt.registerTask('test', [ 'doctool' ]);
+};
